c09ef636b0af97c09981672de58db69a2d832004
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-10-27 11:10:37 -0300

    models

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # to-do-list
+
+*Database diagram: https://imgur.com/a/Sr7xBWt
+*The database is running on AlwaysData
+*Database engine: mySQL
+*ORM: Sequelize
+*Backend: node.JS 14.16.1
+*Frontend: react.JS
--- backend/src/database/models/note.js
+++ backend/src/database/models/note.js
@@ -1,3 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-    
+    let alias = "notas";
+
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        folder_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+        users_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        checked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }
 }
